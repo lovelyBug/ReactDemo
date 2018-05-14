be008f2a6ec85c839246427259ce21052c2f7857
clf_programing <esx56487@qq.com>
2018-05-15 00:18:52 +0800

    clf
    界面框架已经搭好

--- src/login/login.js
+++ src/login/login.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
 import { NavLink } from 'react-router-dom';
+import './login.css';
 class Login extends Component {
+  
+  state={
+    count: 0
+  }
+  handleClick(clickType){
+    switch(clickType){
+      case 'add':
+        this.setState({count: this.state.count + 1});
+        break;
+      case 'del':
+        this.setState({count: this.state.count - 1});
+        break;
+      default:
+        break;
+    }
+  }
   render() {
     return (
-      <div>
-        <NavLink to="/home" exact>
+      <div className="App">
+        {/* <NavLink to="/home" exact>
           <Button type="primary">
             Login!
           </Button>
-        </NavLink>
+        </NavLink> */}
+        <Button type="primary" onClick={()=>{this.handleClick('add')}}>
+            +1
+        </Button>
+        <Button>{this.state.count}</Button>
+        <Button type="primary" onClick={()=>{this.handleClick('del')}}>
+            -1
+        </Button>
       </div>
     );
   }
